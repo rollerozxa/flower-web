f6f5c07b838ee0a8547a59b019357e1df1a227e1
ROllerozxa <rollerozxa@voxelmanip.se>
2019-01-23 21:24:12 +0100

    Fix MySQL database credentials.

--- mysql.php
+++ mysql.php
@@ -1,6 +1,6 @@
 <?php
 // Hacky code to make MySQL work on both of my machines.
-if (__DIR__ == "/home/administrator/Dropbox/www_flowers") {
+if (__DIR__ == "/home/administrator/www_flowers") {
 	$mysqli = new mysqli('127.0.0.1', 'admin', 'cuddles', 'origami');
 } else { 
 	$mysqli = new mysqli('127.0.0.1', 'root', 'cuddles', 'origami');
