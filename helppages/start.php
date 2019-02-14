525549d12f1690a282745202a91db388c03336a7
ROllerozxa <rollerozxa@voxelmanip.se>
2019-02-14 22:21:42 +0100

    Improve the layout of the flower school starting page.

--- helppages/start.php
+++ helppages/start.php
@@ -1,12 +1,13 @@
-Welcome.
+<p>Welcome.</p>
 
-<table>
-	<tr style="width:100%"><td class="box" style="width:100%">
+<table class="fullwidth">
+	<tr><td class="flowbox">
 		1. <a href="flowerschool.php?page=1">Seeds and Stars</a>
 		<p>This part introduces you into the game and makes you familiar with its currency.</p>
 	</td></tr>
-	<tr><td class="box">
+	<tr><td class="flowbox">
 		2. <a href="flowerschool.php?page=2">Items and Resources</a>
 		<p>This part will talk about taking care of your flower.</p>
 	</td></tr>
+	<tr><td class="flowbox"></td></tr>
 </table>
