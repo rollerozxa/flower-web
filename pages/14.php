3c1112d28d8f0f5a7740395bd56fb0cc5986407c
ROllerozxa <rollerozxa@voxelmanip.se>
2019-03-06 13:08:54 +0100

    Improve the country list.

--- pages/14.php
+++ pages/14.php
@@ -9,14 +9,9 @@ Change nickname to:<br>
 Change flag:<br>
 <?php
 // Order countries in alphabetical order (a la the in-game change flag system)
-$countries_alpha = array();
-foreach ($countries as $country) {
-	$countryinfo = explode("@", $country);
-	array_push($countries_alpha, array($countryinfo[0],$countryinfo[1]));
-}
-usort($countries_alpha, function($a, $b) { return strcmp($a[1], $b[1]); });
+usort($countries, function($a, $b) { return strcmp($a[1], $b[1]); });
 
-foreach ($countries_alpha as $country) {
+foreach ($countries as $country) {
 	echo '<a class="flaglink" href="' . alink(11,'changeflag',$country[0]) . '"><div class="flagbox"><img class="f" src="flags/' . $country[0] . '.png"> ' . $country[1] . '</div></a>';
 }
 ?>
