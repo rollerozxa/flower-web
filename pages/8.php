dd4360d282f2b6dae3012206fda1c1db83d0c271
ROllerozxa <rollerozxa@voxelmanip.se>
2019-03-09 10:04:52 +0100

    Implement deleting mail.

--- pages/8.php
+++ pages/8.php
@@ -1,11 +1,10 @@
 <?php
 $db_query = SqlQuery("SELECT * FROM inbox WHERE recipient_id = '" . $userdata['userID'] . "' ORDER BY mailID DESC;");
-$bg = 0;
 while ($record = mysqli_fetch_array($db_query)) {
 	?><div class="box inbox">
 	<table style="width:100%;height:100%;">
-		<tr><td class="inboxcell1"><strong>From: <?php echo $record['sender_name']; ?></strong><p><?php echo $record['message'] ?></p></td></tr>
-		<tr><td class="inboxcell2"><a href="delet">Delete this message</a></td></tr>
+		<tr><td class="inboxcell1"><strong>From: <?=$record['sender_name']; ?></strong><p><?=$record['message'] ?></p></td></tr>
+		<tr><td class="inboxcell2"><a href="<?=alink(8,'deletemail',$record['mailID']) ?>">Delete this message</a></td></tr>
 	</table>
 	</div><?php
 }
--- pages/a.php
+++ pages/a.php
@@ -269,6 +269,22 @@ switch ($_REQUEST['a']) {
 			header_msg("You don't have enough stars!", "ff7777");
 		}
 	break;
+
+	///
+	/// 8 - Inbox
+	///
+	case 'deletemail':
+		if (is_numeric($quantity)) {
+			$maildata = SqlQueryFetchRow("SELECT * FROM inbox WHERE mailID = $quantity;");
+			if ($maildata['recipient_id'] == $userdata['userID']) {
+				SqlQuery("DELETE FROM inbox WHERE mailID = $quantity");
+			} else {
+				header_msg("This isn't your mail!", "ff7777");
+			}
+		} else {
+			header_msg("...Huh?", "ff7777");
+		}
+	break;
 	
 	///
 	/// 10 - Global Compost
