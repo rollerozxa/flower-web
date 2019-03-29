c70685fa1b6ba3b1708f3715ac43f7f934e42358
ROllerozxa <rollerozxa@voxelmanip.se>
2019-03-29 22:04:40 +0100

    Redesign settings page.

--- pages/13.php
+++ pages/13.php
@@ -1,35 +1,28 @@
 <p class="title">Settings</p>
 
-<div class="box optbox" style="background-color:#ffcccc">
-	<span class="title">Yellowish background</span>
-	<form method="POST">
-		<input type="hidden" name="a" value="yelbak">
-		<input type="radio" name="yelbak" id="yelbak_no" value="0"<?=($userdata['yellow_background'] == 0 ? ' checked' : '')?>> <label for="yelbak_no">Disabled</label>
-		<input type="radio" name="yelbak" id="yelbak_yes" value="1"<?=($userdata['yellow_background'] == 1 ? ' checked' : '')?>> <label for="yelbak_yes">Enabled</label>
-		<br><input type="submit" value="Submit">
-	</form>
-	<i>This gives the background a more yellowish color, which might feel better on the eyes.</i>
-</div>
-
-<div class="box optbox" style="background-color:#ffcccc">
-	<span class="title">Menu bar style</span>
-	<form method="POST">
-		<input type="hidden" name="a" value="menustyle">
-		<input type="radio" name="menustyle" id="menustyle_buttons" value="0"<?=($userdata['menustyle'] == 0 ? ' checked' : '')?>> <label for="menustyle_buttons">Buttons</label>
-		<input type="radio" name="menustyle" id="menustyle_links" value="1"<?=($userdata['menustyle'] == 1 ? ' checked' : '')?>> <label for="menustyle_links">Links</label>
-		<input type="radio" name="menustyle" id="menustyle_dropdown" value="2"<?=($userdata['menustyle'] == 2 ? ' checked' : '')?>> <label for="menustyle_dropdown">Dropdown</label>
-		<br><input type="submit" value="Submit">
-	</form>
-	<i>Change the style of the buttons in the menu bar.</i>
-</div>
-
-<div class="box optbox" style="background-color:#ffcccc">
-	<span class="title">Zoom menu</span>
-	<form method="POST">
-		<input type="hidden" name="a" value="zoom">
-		<input type="radio" name="zoom" id="zoom_no" value="0"<?=($userdata['zoom'] == 0 ? ' checked' : '')?>> <label for="zoom_no">Disabled</label>
-		<input type="radio" name="zoom" id="zoom_yes" value="1"<?=($userdata['zoom'] == 1 ? ' checked' : '')?>> <label for="zoom_yes">Enabled</label>
-		<br><input type="submit" value="Submit">
-	</form>
-	<i>This toggles the zoom menu which allows you to set a zoom level for the page.</i>
-</div>
+<form method="POST"><input type="hidden" name="a" value="setoptions"><table class="opt_tbl">
+	<tr>
+		<td class="a">Yellowish Background</td>
+		<td class="b">
+			<input type="radio" name="yelbak" id="yelbak_no" value="0"<?=($userdata['yellow_background'] == 0 ? ' checked' : '')?>> <label for="yelbak_no">Disabled</label>
+			<input type="radio" name="yelbak" id="yelbak_yes" value="1"<?=($userdata['yellow_background'] == 1 ? ' checked' : '')?>> <label for="yelbak_yes">Enabled</label>
+		</td>
+		<td class="c">This gives the background a more yellowish color, which might feel better on the eyes.</td>
+	</tr><tr>
+		<td class="a">Menu bar style</td>
+		<td class="b">
+			<input type="radio" name="menustyle" id="menustyle_buttons" value="0"<?=($userdata['menustyle'] == 0 ? ' checked' : '')?>> <label for="menustyle_buttons">Buttons</label>
+			<input type="radio" name="menustyle" id="menustyle_links" value="1"<?=($userdata['menustyle'] == 1 ? ' checked' : '')?>> <label for="menustyle_links">Links</label>
+			<input type="radio" name="menustyle" id="menustyle_dropdown" value="2"<?=($userdata['menustyle'] == 2 ? ' checked' : '')?>> <label for="menustyle_dropdown">Dropdown</label>
+		</td>
+		<td class="c">Change the style of the buttons in the menu bar.</td>
+	</tr><tr>
+		<td class="a">Zoom menu</td>
+		<td class="b">
+			<input type="radio" name="zoom" id="zoom_no" value="0"<?=($userdata['zoom'] == 0 ? ' checked' : '')?>> <label for="zoom_no">Disabled</label>
+			<input type="radio" name="zoom" id="zoom_yes" value="1"<?=($userdata['zoom'] == 1 ? ' checked' : '')?>> <label for="zoom_yes">Enabled</label>
+		</td>
+		<td class="c">This toggles the zoom menu which allows you to set a zoom level for the page.</td>
+	</tr>
+	<tr><td colspan=3 class="a"><input type="submit" value="Submit"></td></tr>
+</table></form>
--- pages/action/13.php
+++ pages/action/13.php
@@ -3,26 +3,15 @@
 ///
 /// 13 - Settings
 ///
+
 switch ($_REQUEST['a']) {
-	case 'yelbak':
-		if ($_POST['yelbak'] == 1 || $_POST['yelbak'] == 0) {
-			SqlQuery("UPDATE `user` SET `yellow_background` = {$_POST['yelbak']} WHERE `uid` = '$uid'");
-		} else {
-			header_msg("Invalid value for option 'Yellowish Background'.", "ff7777");
-		}
-	break;
-	case 'menustyle':
-		if ($_POST['menustyle'] == 2 || $_POST['menustyle'] == 1 || $_POST['menustyle'] == 0) {
-			SqlQuery("UPDATE `user` SET `menustyle` = {$_POST['menustyle']} WHERE `uid` = '$uid'");
-		} else {
-			header_msg("Invalid value for option 'Menu bar style'.", "ff7777");
-		}
-	break;
-	case 'zoom':
-		if ($_POST['zoom'] == 1 || $_POST['zoom'] == 0) {
-			SqlQuery("UPDATE `user` SET `zoom` = {$_POST['zoom']} WHERE `uid` = '$uid'");
-		} else {
-			header_msg("Invalid value for option 'Zoom'.", "ff7777");
-		}
+	case 'setoptions':
+		if ($_POST['yelbak'] != 1 && $_POST['yelbak'] != 0) $_POST['yelbak'] = 0;
+		if ($_POST['menustyle'] != 2 && $_POST['menustyle'] != 1 && $_POST['menustyle'] != 0) $_POST['menustyle'] = 0;
+		if ($_POST['zoom'] != 1 && $_POST['zoom'] != 0) $_POST['zoom'] = 1;
+
+		SqlQuery("UPDATE user SET
+			yellow_background = {$_POST['yelbak']}, menustyle = {$_POST['menustyle']}, zoom = {$_POST['zoom']}
+			WHERE uid = '$uid'");
 	break;
 }
