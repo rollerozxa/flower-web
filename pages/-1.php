e5ae2894861b7d0654cdf0991495e7a5316b62a4
ROllerozxa <rollerozxa@voxelmanip.se>
2019-04-17 21:51:17 +0200

    Message editing page fixes.

--- pages/-1.php
+++ pages/-1.php
@@ -1,13 +1,16 @@
 <?php
-$messageid = (isset($_GET['messageid']) && is_numeric($_GET['messageid']) ? $_GET['messageid'] : '');
+$messageid = (isset($_GET['messageid']) ? $_GET['messageid'] : '');
 
-if ($messageid != '') {
-	$chatmessage = fetch("SELECT * FROM chat WHERE ID = ?", [$messageid]);
+$chatmessage = fetch("SELECT * FROM chat WHERE ID = ?", [$messageid]);
+
+if ($chatmessage) {
 	?><form action="<?=pagelink(2)?>" method="POST">
 	<input type="hidden" name="a" value="editmessage">
 	<input type="hidden" name="message_id" value="<?=$messageid ?>">
 	<input type="text" name="edited_message" value="<?=$chatmessage['message'] ?>">
 	<input type="submit" value="Submit">
 	</form><?php
+} else {
+	echo "Message doesn't exist.";
 }
 ?>
