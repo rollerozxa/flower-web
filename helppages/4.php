5c50c4b0664b7f8f1bbbb6bfbfaca9f0877d0fa6
ROllerozxa <rollerozxa@voxelmanip.se>
2019-04-20 21:02:57 +0200

    Fix up flower school page 4.

--- helppages/4.php
+++ helppages/4.php
@@ -4,15 +4,15 @@
 
 <p>You can either choose to BUY <span class="starc">Stars</span> using <span class="seedc">Seeds</span> or to SELL <span class="starc">Stars</span> for <span class="seedc">Seeds</span>. This is because <span class="starc">Stars</span> are considered to be a more valuable currency than <span class="seedc">Seeds</span>.</p>
 
-<p>The price to buy and sell <span class="starc">Stars</span> wildly varies depending on the supply and demand of the currencies. This however doesn't mean that it will be </p>
+<p>The price to buy and sell <span class="starc">Stars</span> wildly varies depending on the supply and demand of the currencies.</p>
 
 <p>And that's it. You can either go to the next part or you can solve this simple math problem:</p>
 
-<p><em>Someone has O bananas and W apples. What would a box with that amount of bananas in length, that amount of apples in width, and that amount of bananas in height be in size? (1 food item = 1 cm)</em></p>
+<p><em>A box is O cm<sup>3</sup> in length, w cm<sup>3</sup> in width, and O cm<sup>3</sup> in height. What's the volume of the box, in cubic centimeters?</em></p>
 
 <p id="owo"><button onclick="showOwOanswer()">Show Answer</button></p>
 
-<p id="whatsthis" style="display:none">Answer: OwO (cm<sup>3</sup>)</p>
+<p id="whatsthis" style="display:none">Answer: OwO cm<sup>3</sup></p>
 
 <script>
 function showOwOanswer() {
