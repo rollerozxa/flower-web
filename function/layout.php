5c05e38ca792ed8f539e14d18fbc860f5b3f76c4
ROllerozxa <rollerozxa@voxelmanip.se>
2019-05-20 21:28:49 +0200

    Document fieldselect().

--- function/layout.php
+++ function/layout.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Create a dropdown field, used for forms.
+ *
+ * @param string $field  Name of the dropdown.
+ * @param mixed $checked Value of the key of the selected item.
+ * @param array $choices List of items in the dropdown.
+ * @param bool $submitonchange Should the form be submitted when changed?
+ * @return string HTML code of the dropdown field.
+ */
 function fieldselect($field, $checked, $choices, $submitonchange) {
 	$out = sprintf('<select name="%s" %s>',$field,($submitonchange ? 'onchange="this.form.submit()"' : ''));
 	foreach ($choices as $key => $val) {
