d0ee047517d30e18cde608e45b5cb5113d0cb847
ROllerozxa <rollerozxa@voxelmanip.se>
2019-06-07 22:03:40 +0200

    Here we either use sprintfs or we don't!

--- pages/420.php
+++ pages/420.php
@@ -23,8 +23,8 @@ $admintools = [
 	], 'impersonate' => [
 		'content' => sprintf(
 			 '<span class="title">Impersonate someone! &Ograve;w&Oacute;</span>'
-			.'<br><br><form method="GET">'.$form.'</form>'
-		),
+			.'<br><br><form method="GET">%s</form>',
+		$form),
 		'powerlevel' => 4
 	]
 ];
