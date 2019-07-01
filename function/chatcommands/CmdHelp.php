3dd746595d9786b178b29ad060eb96bc313bf3c1
ROllerozxa <rollerozxa@voxelmanip.se>
2019-07-01 21:52:01 +0200

    Update list of commands in /help.

--- function/chatcommands/CmdHelp.php
+++ function/chatcommands/CmdHelp.php
@@ -1,6 +1,6 @@
 <strong>Chatterbox commands:</strong><br>
 <ul class="nobreak">
-	<li>/brag - Show statistics.</li>
 	<li>/funfact - Show a fun fact (Same as those on the index page).</li>
+	<li>/help - Display this message.</li>
 	<li>/rainbow [message] - Make [message] colorful!</li>
 </ul>
