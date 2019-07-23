7c693d069280ff4717e59a98e3d6bdc9efe9c950
ROllerozxa <rollerozxa@voxelmanip.se>
2019-07-23 22:01:44 +0200

    Lick your ice cream, not your zoom menu buttons!

--- function/layout.php
+++ function/layout.php
@@ -38,7 +38,7 @@ function zoom_menu() {
 	$out = '<div class="box zoom">Zoom: ';
 	foreach ($zoom_levels as $k => $v) {
 		$out .= sprintf(
-			'<button onlick="%s">%s</button> ',
+			'<button onclick="%s">%s</button> ',
 		$k, $v);
 	}
 	$out .= '</div>';
