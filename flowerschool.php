5465d93e4b1b33e016f8ec7b5e028321f9ff42d3
ROllerozxa <rollerozxa@voxelmanip.se>
2019-08-02 21:34:42 +0200

    Add a 404 message in flowerschool.php.

--- flowerschool.php
+++ flowerschool.php
@@ -14,9 +14,15 @@ $page = (isset($_GET['page']) ? $_GET['page'] : 'start');
 		<script src="assets/loaded.js"></script>
 		<div class="box outer" style="background-color:#eeeeee">
 			<?php
-			include('helppages/'.$page.'.php');
-			if ($page != 'start') {
-				echo '<a class="bottomtext" href="flowerschool.php">&lt; Go back</a>';
+			if (file_exists('helppages/'.$page.'.php')) {
+				include('helppages/'.$page.'.php');
+				// TODO: Possibly move this outside of the if statement? Dunno really.
+				//		 I mean, if someone managed to change the page name, they probably don't need a link back.
+				if ($page != 'start') {
+					echo '<a class="bottomtext" href="flowerschool.php">&lt; Go back</a>';
+				}
+			} else {
+				echo '<center><h1>404</h1>This page doesn\'t exist.</center>';
 			}
 			?>
 		</div>
