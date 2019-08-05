c1459637f99ecf5504823b2da2f51fd7002292bb
ROllerozxa <rollerozxa@voxelmanip.se>
2019-08-05 21:46:01 +0200

    Minor work on stars draw stuff.

--- pages/action/6.php
+++ pages/action/6.php
@@ -7,6 +7,10 @@
 
 switch ($_REQUEST['a']) {
 	case 'buystartickets':
-		// TODO!
+		if ($quantity * 2 <= $userdata['stars']) {
+			header_msg("Bought $quantity stars draw tickets for " . ($quantity * 2) . " stars.");
+		} else {
+			header_msg("You don't have enough stars!", "ff7777");
+		}
 	break;
 }
